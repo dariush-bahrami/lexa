cf005fe562bf465cceef0b83321626f7f2531142
Dariush Bahrami <dariushbahrami@gmail.com>
2021-10-27 03:57:03 +0330

    black formatted

--- lexa/fluids/__init__.py
+++ lexa/fluids/__init__.py
@@ -1,2 +1,7 @@
-from . import (friction_velocity, reynolds, skin_friction_coefficient,
-               wall_shear_stress, y_plus)
+from . import (
+    friction_velocity,
+    reynolds,
+    skin_friction_coefficient,
+    wall_shear_stress,
+    y_plus,
+)
